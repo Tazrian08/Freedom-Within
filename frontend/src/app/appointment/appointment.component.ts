30e959a18c4f69adcba7a9fe55949459f4f83f30
Tazrian Hossain <144312101+Tazrian08@users.noreply.github.com>
2024-01-10 20:36:44 +0600

    Appointment start

--- frontend/src/app/appointment/appointment.component.ts
+++ frontend/src/app/appointment/appointment.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-appointment',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment.component.css']
 })
 export class AppointmentComponent {
+
+  constructor(private http: HttpClient, private router: Router) 
+  {
+    { 
+    
+      this.http.get("http://localhost:8000/api/allservice")
+    
+      .subscribe((resultData: any)=>
+      {
+        this.services=resultData
+        console.log(this.services)
+      });
+    }
+
+
+  { 
+    
+    this.http.get("http://localhost:8000/api/alltherapist")
+  
+    .subscribe((resultData: any)=>
+    {
+      this.therapists=resultData
+      console.log(this.therapists)
+    });
+  }
+
+  }
+
+  services:any
+  therapists:any
+
 
 }
