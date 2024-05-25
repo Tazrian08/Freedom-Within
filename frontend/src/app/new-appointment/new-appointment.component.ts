cef21d21d70aecffb791de1c97152e72a70eaeec
Wasima Annan <143730734+wasimaannan@users.noreply.github.com>
2024-05-25 15:57:28 +0600

    fix

--- frontend/src/app/new-appointment/new-appointment.component.ts
+++ frontend/src/app/new-appointment/new-appointment.component.ts
@@ -113,7 +113,8 @@ export class NewAppointmentComponent {
 
     this.http.post("http://localhost:8000/api/reappointment", formData).subscribe((resultData: any) => {
       console.log(resultData)
-      // this.router.navigate(['admin/login']);
+      alert("Appointment Made. You will be contacted soon for confirmation")
+      this.router.navigate(['home']);
     });
 
 
