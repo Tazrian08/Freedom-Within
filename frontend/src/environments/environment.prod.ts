86ea7cf7a5afc17d5cbb38000606794e9ef7d92e
Tazrian Hossain <144312101+Tazrian08@users.noreply.github.com>
2025-05-09 02:45:07 +0600

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://your-production-api.com/api' // Production API URL
+  apiBaseUrl: 'https://freedom-within.onrender.com/api' // Production API URL
 };
